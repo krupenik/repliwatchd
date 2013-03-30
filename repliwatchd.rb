3b55f5504f296034606135033f6a8a0a6bf8abf4
Dennis Krupenik <dennis@krupenik.com>
2013-03-30 15:38:06 +0200

    refactoring

--- repliwatchd.rb
+++ repliwatchd.rb
@@ -8,6 +8,12 @@ PIDFILE = '/var/run/repliwatchd.pid'
 LOGFILE = '/var/log/repliwatchd.log'
 MAX_INTERVAL = 60
 
+def generic_log message
+  open(LOGFILE, 'a') do |f|
+    f.printf("%s %s[%d]: %s\n" % [Time.now.strftime("%c"), $0, Process.pid, message.to_s])
+  end
+end
+
 class RepliWatchDaemon
   def initialize
     @is_running = true
@@ -15,31 +21,53 @@ class RepliWatchDaemon
   end
 
   def start
-    if ::Process.fork then exit!(0); end
-    ::Process.setsid
+    if Process.fork then exit!(0); end
+    Process.setsid
 
     $stdin.reopen("/dev/null", "r")
     $stdout.reopen("/dev/null", "w")
     $stderr.reopen("/dev/null", "w")
 
     open(PIDFILE, 'w') { |f| f.puts(Process.pid) }
-    at_exit { ::File.unlink(PIDFILE) }
+    at_exit { File.unlink(PIDFILE) }
     self.serve
   end
 
   def connect_to_database
-    ini = ::IniFile.load("#{ENV["HOME"]}/.my.cnf")
+    ini = IniFile.load("#{ENV["HOME"]}/.my.cnf")
     begin
-      @dbh = ::Mysql2::Client.new(username: ini['client']['user'], password: ini['client']['password'])
-    rescue ::Mysql2::Error
+      @dbh = Mysql2::Client.new(username: ini['client']['user'], password: ini['client']['password'])
+    rescue Mysql2::Error
       sleep @interval
       retry
     end
   end
 
   def log message
-    open(LOGFILE, 'a') do |f|
-      f.printf("%s %s[%d]: %s\n" % [::Time.now.strftime("%c"), $0, ::Process.pid, message.to_s])
+    generic_log message
+  end
+
+  def skip_statements n
+    @dbh.query('stop slave')
+    @dbh.query('set global sql_slave_skip_counter = %d' % n)
+    @dbh.query('start slave')
+  end
+
+  def check_sql_running slave_status
+    if 'yes' != slave_status['Slave_SQL_Running'].downcase
+      if [1032, 1062].include? slave_status['Last_SQL_Errno'].to_i
+        skip_statements 1
+        @interval = 0
+      end
+
+      log slave_status['Last_SQL_Error']
+    end
+  end
+
+  def check_io_running slave_status
+    if 'yes' != slave_status['Slave_IO_Running'].downcase
+      log slave_status['Last_IO_Error']
+      @interval = 0
     end
   end
 
@@ -49,20 +77,9 @@ class RepliWatchDaemon
 
     while @is_running
       slave_status = @dbh.query('show slave status').each { |row| row }[0]
-      if 'yes' != slave_status['Slave_SQL_Running'].downcase
-        if [1032, 1062].include?(slave_status['Last_SQL_Errno'].to_i)
-          @dbh.query('stop slave')
-          @dbh.query('set global sql_slave_skip_counter = 1')
-          @dbh.query('start slave')
-          @interval = 0
-        else
-        end
-        log(slave_status['Last_SQL_Error'])
-      end
 
-      if 'yes' != slave_status['Slave_IO_Running'].downcase
-        log(slave_status['Last_IO_Error'])
-      end
+      check_sql_running slave_status
+      check_io_running slave_status
 
       sleep @interval = (@interval += 1) > MAX_INTERVAL ? MAX_INTERVAL : @interval
     end
@@ -72,8 +89,6 @@ end
 begin
   RepliWatchDaemon.new.start
 rescue => e
-  open(LOGFILE, "a") do |f|
-    f.printf("%s %s[%d]: %s\n" % [::Time.now.strftime("%c"), $0, ::Process.pid, e.to_s])
-  end
+  generic_log e
 end
 
